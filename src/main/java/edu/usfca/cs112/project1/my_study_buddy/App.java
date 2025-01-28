f8b460f0beb01c1674fdb80c71d6efa4ce4adb23
koreyes <klreyes4@dons.usfca.edu>
2025-01-28 13:18:56 -0800

    first commit

---
+++ src/main/java/edu/usfca/cs112/project1/my_study_buddy/App.java
@@ -0,0 +1,10 @@
+package edu.usfca.cs112.project1.my_study_buddy;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
